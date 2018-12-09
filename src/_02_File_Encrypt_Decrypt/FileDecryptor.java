7f4604614bffb4e3dc769192606651f7bc25d320
DanielR1 <activatingredstone@gmail.com>
2018-12-09 12:14:48 -0800

    One more (IP)
    Finished Decryptor and (probably)ToDoList. Just have oen more to go

--- src/_02_File_Encrypt_Decrypt/FileDecryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileDecryptor.java
@@ -1,6 +1,53 @@
 package _02_File_Encrypt_Decrypt;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class FileDecryptor {
 	// Create a program that opens the file created by FileEncryptor and display
 	// the decrypted message to the user in a JOptionPane.
+	public static void main(String[] args) {
+		BufferedReader br;
+		try {
+			String r = "";
+			br = new BufferedReader(new FileReader("src/_02_File_Encrypt_Decrypt/encrypt.txt"));
+			String s = br.readLine();
+			for (int i = 0; i < s.length()/2; i++) {
+				int add = hexToInt(s.substring(2*i, 2*i+2));
+				r+=((char)add);
+				
+			}
+			JOptionPane.showMessageDialog(null, r);
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
+		
+
+	}
+
+	public static int hexToInt(String s) {
+	int first = Integer.parseInt(s.substring(0, 1)) *16;
+	int second=0;
+
+	try {
+		second = Integer.parseInt(s.substring(1,2));
+	}
+	catch (NumberFormatException e){
+		char c1 = s.charAt(1);
+		Character c = Character.valueOf(c1);
+		second=(int)c -55;
+	}
+	return first+second;
+	}
+
 }
--- src/_03_To_Do_List/ToDoList.java
+++ src/_03_To_Do_List/ToDoList.java
@@ -1,24 +1,193 @@
 package _03_To_Do_List;
 
-public class ToDoList {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class ToDoList implements ActionListener {
+	JFrame frame;
+	JPanel panel;
+	JButton add;
+	JButton view;;
+	JButton remove;
+	JButton save;
+	JButton load;
+	ArrayList<String> currentList = new ArrayList<String>();
+
 	/*
-	 * Create a program with five buttons, add task, view tasks, remove task, save list, and load list. 
+	 * Create a program with five buttons, add task, view tasks, remove task, save
+	 * list, and load list.
 	 * 
-	 * When add task is clicked:
-	 * 		ask the user for a  task and save it to an array list
+	 * When add task is clicked: ask the user for a task and save it to an array
+	 * list
 	 * 
-	 * When the view tasks button is clicked:
-	 *		show all the tasks in the list
+	 * When the view tasks button is clicked: show all the tasks in the list
 	 * 
-	 * When the remove task button is clicked:
-	 * 		prompt the user for which task to remove and take it off of the list.
+	 * When the remove task button is clicked: prompt the user for which task to
+	 * remove and take it off of the list.
 	 * 
-	 * When the save list button is clicked:
-	 * 		Save the list to a file
+	 * When the save list button is clicked: Save the list to a file
 	 * 
-	 * When the load list button is clicked:
-	 * 		Prompt the user for the location of the file and load the list from that file
+	 * When the load list button is clicked: Prompt the user for the location of the
+	 * file and load the list from that file
 	 * 
-	 * When the program starts, it should automatically load the last saved file into the list.
+	 * When the program starts, it should automatically load the last saved file
+	 * into the list.
 	 */
+	public void setup() {
+		frame = new JFrame();
+		panel = new JPanel();
+		frame.add(panel);
+		add = new JButton();
+		view = new JButton();
+		remove = new JButton();
+		save = new JButton();
+		load = new JButton();
+		add.setText("Add Task");
+		view.setText("View Tasks");
+		remove.setText("Remove Task");
+		save.setText("Save List");
+		load.setText("Load LIst");
+		add.addActionListener(this);
+		view.addActionListener(this);
+		remove.addActionListener(this);
+		save.addActionListener(this);
+		load.addActionListener(this);
+		panel.add(add);
+		panel.add(view);
+		panel.add(remove);
+		panel.add(save);
+		panel.add(load);
+		frame.pack();
+		panel.setVisible(true);
+		frame.setVisible(true);
+		currentList = new ArrayList<String>();
+		String fileName="";
+		try {
+			BufferedReader br2 = new BufferedReader(new FileReader("src/_03_To_Do_List/IfYouTouchThisYouWillBreakTheCode.txt"));
+			fileName=br2.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			int i =0;
+			BufferedReader br = new BufferedReader(new FileReader("src/_03_To_Do_List/"+fileName+".txt"));
+			String line=br.readLine();
+			currentList.add(line);
+			while(line != null){
+				line=br.readLine();
+				currentList.add(line);
+				i++;
+			}
+			currentList.remove(i);
+			
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		ToDoList t = new ToDoList();
+		t.setup();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == add) {
+			currentList.add(JOptionPane.showInputDialog("What task would you like to add?"));
+		}
+		if (e.getSource() == view) {
+			String v="";
+			for (int i = 0; i < currentList.size(); i++) {
+				v+="\n"+(i+1)+".) "+currentList.get(i);
+			}
+			JOptionPane.showMessageDialog(null, "Your current list is:"+v);
+		}
+		if (e.getSource() == remove) {
+			String v="";
+			for (int i = 0; i < currentList.size(); i++) {
+				v+="\n"+(i+1)+".) "+currentList.get(i);
+			}
+			String input = JOptionPane.showInputDialog("Your current list is:"+v+"\nWhich item would you like to remove? (please only give numbers)");
+			int r = Integer.parseInt(input)-1;
+			currentList.remove(r);
+			
+		}
+		if (e.getSource() == save) {
+			String name = JOptionPane.showInputDialog("Please enter a file name.");
+			String v="";
+			v+=currentList.get(0);
+			for (int i = 1; i < currentList.size(); i++) {
+				v+="\n"+currentList.get(i);
+			}
+			try {
+				FileWriter fw = new FileWriter("src/_03_To_Do_List/"+name+".txt");
+				fw.write(v);
+				fw.close();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			try {
+				FileWriter fw2 = new FileWriter("src/_03_To_Do_List/IfYouTouchThisYouWillBreakTheCode.txt");
+				fw2.write(name);
+				fw2.close();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+
+		if (e.getSource() == load) {		
+			
+			JFileChooser jfc = new JFileChooser();
+			int returnVal = jfc.showOpenDialog(null);
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				String fileName = jfc.getSelectedFile().getAbsolutePath();
+				//System.out.println(fileName);
+				currentList = new ArrayList<String>();
+				try {
+					int i =0;
+					BufferedReader br = new BufferedReader(new FileReader(fileName));
+					String line=br.readLine();
+					currentList.add(line);
+					while(line != null){
+						line=br.readLine();
+						currentList.add(line);
+						i++;
+					}
+					currentList.remove(i);
+					
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+			
+			
+			
+		}
+
+	}
 }
